7a5a6ace1ddb1d5e1f56a0404d66b52709759d5a
RaistlinD <Johnd0712@gmail.com>
2018-07-14 00:23:16 +0800

    ok

--- src/main/java/cn/krynn/order/controller/ClientController.java
+++ src/main/java/cn/krynn/order/controller/ClientController.java
@@ -1,10 +1,14 @@
 package cn.krynn.order.controller;
 
 import cn.krynn.order.client.ProductClient;
+import cn.krynn.order.dataobject.ProductInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by RaistlinD
@@ -23,5 +27,12 @@ public class ClientController {
         String response = productClient.productMsg();
         log.info("response={}", response);
         return response;
+    }
+
+    @GetMapping("/getProductList")
+    public String getProductList() {
+        List<ProductInfo> productInfoList = productClient.listForOrder(Arrays.asList("164103465734242707"));
+        log.info("response={}", productInfoList);
+        return "ok";
     }
 }
---
+++ src/main/java/cn/krynn/order/dataobject/ProductInfo.java
@@ -0,0 +1,40 @@
+package cn.krynn.order.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by RaistlinD
+ * 2018/7/7 下午9:20
+ */
+@Data
+@Entity
+//@Table(name="xxx_xxx")
+public class ProductInfo {
+
+    @Id
+    private String productId;
+
+    private String productName;
+
+    private BigDecimal productPrice;
+
+    private Integer productStock;
+
+    private String productDescription;
+
+    private String productIcon;
+
+    private Integer productStatus;
+
+    private Integer categoryType;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+}
