67c3c6ac3a2a259ad9c65fb543bf73fbdd8e61bd
RaistlinD <Johnd0712@gmail.com>
2018-10-01 23:51:08 +0800

    add ribbitmq client

--- src/main/java/cn/krynn/order/message/StreamClient.java
+++ src/main/java/cn/krynn/order/message/StreamClient.java
@@ -13,10 +13,17 @@ public interface StreamClient {
 
     String INPUT = "myMessage";
 
+    String INPUT2 = "myMessage2";
+
     @Input(StreamClient.INPUT)
     SubscribableChannel input();
 
     @Output(StreamClient.INPUT)
     MessageChannel output();
 
+    @Input(StreamClient.INPUT2)
+    SubscribableChannel input2();
+
+    @Output(StreamClient.INPUT2)
+    MessageChannel output2();
 }
--- src/main/java/cn/krynn/order/message/StreamReceiver.java
+++ src/main/java/cn/krynn/order/message/StreamReceiver.java
@@ -4,6 +4,7 @@ import cn.krynn.order.dto.OrderDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 /**
@@ -22,10 +23,18 @@ public class StreamReceiver {
 
     /**
      * 接收orderDTO对象 消息
+     *
      * @param message
      */
     @StreamListener(StreamClient.INPUT)
-    public void process(OrderDTO message) {
+    @SendTo(StreamClient.INPUT2)
+    public String process(OrderDTO message) {
         log.info("StreamReceiver: {}", message);
+        return "received.";
+    }
+
+    @StreamListener(StreamClient.INPUT2)
+    public void process2(String message) {
+        log.info("StreamReceiver2: {}", message);
     }
 }
