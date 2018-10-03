00638b742e5a8793de21a5f8847d0525fb1e7f16
RaistlinD <Johnd0712@gmail.com>
2018-10-03 22:42:23 +0800

    add ribbitmq client

--- src/main/java/cn/krynn/order/message/ProductInfoReceiver.java
+++ src/main/java/cn/krynn/order/message/ProductInfoReceiver.java
@@ -1,5 +1,8 @@
 package cn.krynn.order.message;
 
+import cn.krynn.order.dataobject.ProductInfoOutput;
+import cn.krynn.order.utils.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -9,10 +12,15 @@ import org.springframework.stereotype.Component;
  * 2018/10/3 12:35 AM
  */
 @Component
+@Slf4j
 public class ProductInfoReceiver {
 
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     public void process(String message) {
-        
+        ProductInfoOutput productInfoOutput = (ProductInfoOutput) JsonUtil.fromJson(message, ProductInfoOutput.class);
+        log.info("从队列【{}】接收到的消息：{}", "productInfo", productInfoOutput);
+
+        //存储到Redis
+
     }
 }
