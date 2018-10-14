4e064564303939b65088e131009d4d55d1f94707
RaistlinD <Johnd0712@gmail.com>
2018-10-14 23:35:56 +0800

    add ribbitmq client

--- src/main/java/cn/krynn/order/OrderApplication.java
+++ src/main/java/cn/krynn/order/OrderApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableFeignClients
 //@SpringBootApplication
 //@EnableDiscoveryClient
 //@EnableCircuitBreaker
 @SpringCloudApplication
+@EnableHystrixDashboard
 public class OrderApplication {
 
     public static void main(String[] args) {
